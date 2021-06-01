d3e04f97edb93d35c318e748dc175707d5a5db2b
arthurmluz <arthurmluz123@gmail.com>
2021-06-01 15:01:05 -0300

    Criação de repositório com o código do bootcamp

---
+++ src/main/java/com/project/bootcamp/exceptions/BusinessException.java
@@ -0,0 +1,10 @@
+package com.project.bootcamp.exceptions;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message){
+        super(message);
+    }
+
+}
+
