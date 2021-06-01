9295ae588c3ffe4d0f622c1071df9a5abe58915b
arthurmluz <arthurmluz123@gmail.com>
2021-06-01 15:38:27 -0300

    alterando versão descrição

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -3,7 +3,6 @@ package com.project.bootcamp;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +17,8 @@ public class BootcampApplication {
 	@Bean
 	public OpenAPI customOpenAPI(){
 		return new OpenAPI().info(new Info()
-				.title("")
-				.version("1.0")
+				.title("Projeto criado para o bootcamp santander dev week")
+				.version("2.0")
 				.termsOfService("https://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("https://springdoc.org")) );
 	}
